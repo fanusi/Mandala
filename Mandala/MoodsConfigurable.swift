70ddf6272b3233f703806235f15132a571af35c8
fanusi <stephanetrouve@hotmail.com>
2022-05-19 10:30:36 +0200

    Versie 1

---
+++ Mandala/MoodsConfigurable.swift
@@ -0,0 +1,12 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala
+//
+//  Created by Stéphane Trouvé on 19/05/2022.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+    func add(_ moodEntry: MoodEntry)
+}
---
+++ MoodEntry.swift
@@ -0,0 +1,13 @@
+//
+//  MoodEntry.swift
+//  Mandala
+//
+//  Created by Stéphane Trouvé on 16/05/2022.
+//
+
+import UIKit
+
+struct MoodEntry {
+    var mood: Mood
+    var timestamp: Date
+ }
